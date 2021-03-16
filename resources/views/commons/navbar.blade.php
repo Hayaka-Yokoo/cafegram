dd408447d614ee7796d7ccb9ffa02a910a35022b
hayaka.yokoo <littgle.manahaya@gmial.com>
2021-03-16 15:56:54 +0900

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class="mb-4">
+        <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: #8b4513;">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" href="/">Cafegram</a>
+        
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- ユーザ登録ページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">新規登録</a></li>
+                {{-- ログインページへのリンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
