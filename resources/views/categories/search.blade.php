1136d67f25ec4fd4fa1ad7f88fc9cc19501d6e48
hayaka.yokoo <littgle.manahaya@gmial.com>
2021-04-07 17:05:50 +0900

    pushagain

--- resources/views/categories/search.blade.php
+++ resources/views/categories/search.blade.php
@@ -7,7 +7,7 @@
         <div class="form-group">
             {!! Form::label('category_name', 'カテゴリー：') !!}
             @foreach ($categories as $category)
-                <label>{{ Form::checkbox('category[]', $category->id) }}{{ $category->category_name }}</label>
+                <label>{{ Form::checkbox('category[]', $category->id, false, ['class'=>'mycheckbox']) }}{{ $category->category_name }}</label>
             @endforeach
         </div>
         {!! Form::submit('検索', ['class' => 'btn btn-brown btn-block']) !!}
